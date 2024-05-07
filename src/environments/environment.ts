cc373f98042243536436fba086dd4396201f049c
Javi-Vargas <javiervargas470@gmail.com>
2024-05-07 13:56:56 -0400

    Added Tmrw api values.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,13 @@ export const environment = {
     XRapidAPIHostHeaderName: 'X-RapidAPI-Host',
     XRapidAPIHostHeaderValue: 'open-weather13.p.rapidapi.com',
     XRapidAPIKeyHearderName: 'X-RapidAPI-Key',
-    XRapidAPIKeyHeaderValue: 'dd2acea9dfmsh96742d839488ac3p166fc4jsn6802642a06bf'
+    XRapidAPIKeyHeaderValue: 'dd2acea9dfmsh96742d839488ac3p166fc4jsn6802642a06bf',
+
+    weatherApiNewUrl: '',
+    XTmrwdAPIHostHeaderName: 'X-TmrwAPI-Host',
+    XTmrwAPIHostHeaderValue: 'https://api.tomorrow.io/v4/',
+    XTmrwAPIKeyHearderName: 'X-TmrwAPI-Key',
+    XTmrwAPIKeyHeaderValue: '5EV9LrljZe1cF49ZkTQOifTccS2gmtZH',
 
     //https://open-weather13.p.rapidapi.com/city/Winter%20Park/EN
     // Other environment-specific variables
